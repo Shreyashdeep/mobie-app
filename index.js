d2d8ea64740ba60140fac0007a19e7e3bbfba023
Shreyashdeep <shreyashdeep20@gmail.com>
2023-07-17 22:01:57 +0530

    input added to database

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const appSettings= {
-    databaseURL: "https://playgrounf-77642-default-rtdb.firebaseio.com/"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://realtime-database-f0a4d-default-rtdb.firebaseio.com/"
 }
-const inputFieldEl =document.getElementById("input-field")
-const addButtonEl =document.getElementById("add-button")
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click", function() {
-    let inputValue= inputFieldEl.value
-    console.log(inputValue)
+    let inputValue = inputFieldEl.value
+    
+    push(shoppingListInDB, inputValue)
+    
+    inputFieldEl.value = ""
+    
+    shoppingListEl.innerHTML += `<li>${inputValue}</li>`
 })
